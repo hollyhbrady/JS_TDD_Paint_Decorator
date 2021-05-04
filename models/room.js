21c513811c54ec4892e2d9fdadd79ef893642416
hollyhbrady <hollyhbrady@gmail.com>
2021-05-04 15:58:20 +0100

    test 1 passing

--- models/room.js
+++ models/room.js
@@ -2,3 +2,5 @@ const Room = function (square_meters, painted){
     this.square_meters = square_meters;
     this.painted = false;
 };
+
+module.exports = Room;
--- models/specs/room_specs.js
+++ models/specs/room_specs.js
@@ -4,11 +4,11 @@ const Room = require('../room.js');
 describe('Room', function(){
     let room;
     beforeEach(function(){
-        room = new Room('10', false);
+        room = new Room(10, false);
     });
 
     it('should have an area in square meters', function(){
-        const actual = room.square_meter;
-        assert.strictEqual(actual, '10');
+        const actual = room.square_meters;
+        assert.strictEqual(actual, 10);
     });
 });
