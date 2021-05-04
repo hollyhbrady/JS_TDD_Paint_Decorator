d8487e0e81ff80b65fc393fe01837d9ae267a133
hollyhbrady <hollyhbrady@gmail.com>
2021-05-04 16:59:45 +0100

    end point of lab =- working on function for test 8

--- models/decorator.js
+++ models/decorator.js
@@ -1,5 +1,10 @@
 const Decorator = function(stock){
-    this.stock = []
+    this.stock = [];
 };
+
+Decorator.prototype.addCan = function(paint.litres){
+    this.stock.push(paint.litres)
+};
+
 
 module.exports = Decorator;
