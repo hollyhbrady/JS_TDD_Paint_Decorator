fe5a021c0c575572e91ebce949a89bdafc857f06
hollyhbrady <hollyhbrady@gmail.com>
2021-05-04 15:49:06 +0100

    first test written

--- models/specs/room_specs.js
+++ models/specs/room_specs.js
@@ -1,2 +1,14 @@
 const assert = require('assert');
 const Room = require('../room.js');
+
+describe('Room', function(){
+    let room;
+    beforeEach(function(){
+        room = new Room('10', false);
+    });
+
+    it('should have an area in square meters', function(){
+        const actual = room.square_meter;
+        assert.strictEqual(actual, '10');
+    });
+});
